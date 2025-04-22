a67db321b299a323c2bb02377ac0a59eb838af58
Silvia <giardinisil@gmail.com>
2025-04-22 18:10:50 -0300

    Se agregó actividad 7: función para calcular (IMC)

---
+++ 05 Funciones/Actividad_8.py
@@ -0,0 +1,16 @@
+""" Crear una función llamada calcular_imc(peso, altura) que reciba el peso en kilogramos y la altura en metros,
+    y devuelva el índice de masa corporal (IMC). Solicitar al usuario los datos y llamar a la función
+    para mostrar el resultado con dos decimales. """
+
+# Variables para guardar los datos ingresado por el usuario.
+peso = float(input("Ingrese su peso en kilogramos: "))
+altura = float(input("Ingrese su altura en metros: "))
+
+def calcular_imc(peso, altura):
+    imc = peso / (altura ** 2)
+    return imc
+
+# Llamar a la función y mostrar el resultado con dos decimales.
+resultado_imc = calcular_imc(peso, altura)
+print(f"Según los datos ingresados, su IMC es: {resultado_imc:.2f}")
+
