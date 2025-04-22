ff3b35bbb1c0971ae8058ddd4a407da22fe2d548
Silvia <giardinisil@gmail.com>
2025-04-22 18:32:29 -0300

    Modificación de comentarios en actividad 9

--- 05 Funciones/Actividad_9.py
+++ 05 Funciones/Actividad_9.py
@@ -2,15 +2,15 @@
     y devuelva su equivalente en Fahrenheit. Pedir al usuario la temperatura en Celsius y mostrar el resultado
     usando la función. """
 
-# Variables para guardar los datos ingresado por el usuario.
+# Variable para guardar el dato ingresado por el usuario.
 celsius = float(input("Ingrese la temperatura actual en grados celsius: "))
 
-
+#Función que realiza el calculo de conversión
 def celsius_a_fahrenheit(celsius):
     fahrenheit = (celsius * 1.8) +32
     return fahrenheit
 
-# Llamar a la función y mostrar el resultado con dos decimales.
+# Llamar a la función y mostrar el resultado de la conversión.
 en_fahrenheit = celsius_a_fahrenheit(celsius)
 print(f"{celsius} grados celsius equivalen a {en_fahrenheit} fahrenheit")
 
