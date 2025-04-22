220e7f1d4dfdc9c15b0ac85c48266ae201a85d16
Silvia <giardinisil@gmail.com>
2025-04-22 14:04:51 -0300

    Se agregó la actividad 1: función para imprimir "Hola Mundo"

---
+++ 05 Funciones/Actividad_1.py
@@ -0,0 +1,10 @@
+""" Crear una función llamada imprimir_hola_mundo que imprima por pantalla el mensaje: “Hola Mundo!”.
+    Llamar a esta función desde el programa principal. """
+    
+def imprimir_hola_mundo():
+    #Esta función imprime el mensaje 'Hola Mundo!'
+    print("Hola Mundo!")
+
+#Llamar a la función
+imprimir_hola_mundo()
+                   
