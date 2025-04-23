98d0e06c2fcc55a0fe9cfa7c1c91f8a9d1e05911
Silvia <giardinisil@gmail.com>
2025-04-23 17:56:49 -0300

    Se corrigieron errores en actividad 1

--- Listas/Listas_Actividad_1.py
+++ Listas/Listas_Actividad_1.py
@@ -1,10 +1,14 @@
 """ 1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range. """
 
-#Bucle for que recorre un rango del 1 al 101 sin incluirlo.
-for i in range (1,101):
-    #Estructura if para tomar solo los números múltiplos de 4
+# 1) Crear una lista vacía
+multiplos_de_4 = []
+
+# Bucle for que recorre un rango del 1 al 101 (para incluir el 100).
+for i in range(1, 101):
+    # Estructura if para tomar solo los números múltiplos de 4
     if i % 4 == 0:
-        #Guardamos los multiplos de 4 en la variable lista
-        lista = i
-        #Imprimimos la lista
-        print(lista)
+        # 2) Agregamos el múltiplo de 4 a la lista
+        multiplos_de_4.append(i)
+
+# 3) Imprimimos la lista completa
+print(multiplos_de_4)
