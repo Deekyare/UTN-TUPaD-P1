524fb8970887c8481a14fbd5e6f941dc9cfbc96f
Silvia <giardinisil@gmail.com>
2025-04-23 18:07:26 -0300

    Se creó actividad 8: Dobles

---
+++ Listas/Listas_Actividad_8.py
@@ -0,0 +1,11 @@
+""" Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append directamente.
+    Imprimir la lista resultante por pantalla."""
+    
+#Lista vacía
+dobles = []
+#Agregar el doble de 5, 10 y 15 usando append directamente. 
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+#Imprimimos la lista dobles con los nuevos datos.
+print(dobles)
