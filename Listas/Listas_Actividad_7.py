18299f940f63145664a4b6ac252475decf9234a5
Silvia <giardinisil@gmail.com>
2025-04-23 18:01:40 -0300

    Se creó actividad 7: Reemplazar valores centrales.

---
+++ Listas/Listas_Actividad_7.py
@@ -0,0 +1,12 @@
+""" Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores cualesquiera.
+    autos = ["sedan", "polo", "suran", "gol"]"""
+    
+#Lista de autos
+autos = ["sedan", "polo", "suran", "gol"]
+#Imprimimos la lista original
+print(autos)
+#Reemplazar los dos valores centrales (índices 1 y 2) dos nuevos valores cualesquiera. 
+autos[1] = "loro"
+autos[2] = "oso"
+#Imprimimos la lista modificada
+print(autos)
