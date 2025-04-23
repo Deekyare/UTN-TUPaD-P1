81e82dda6268cc56e89caf7659e79a41f1f71c78
Silvia <giardinisil@gmail.com>
2025-04-23 17:09:31 -0300

    Se creó actividad 2: Acceder a un elemento específico de la lista.

---
+++ Listas/Listas_Actividad_2.py
@@ -0,0 +1,11 @@
+""" 2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo.
+    ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números
+    negativos! """
+
+#Lista de elemetos (frutas)
+frutas = ["banana","manzana","pera","frutilla","limón"]
+
+#Primera forma de acceder al penúltimo elemento
+print(frutas[3])
+# Segunda forma
+print(frutas[-2])
