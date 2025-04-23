8459ca2080ec1d5a4a7957619c097b4bfdcb5962
Silvia <giardinisil@gmail.com>
2025-04-23 17:17:46 -0300

    Se creó actividad 3: Agregar elementos a una lista vacía.

---
+++ Listas/Listas_Actividad_3.py
@@ -0,0 +1,13 @@
+""" 3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por pantalla.
+    Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. 
+    Por ejemplo: lista_vacia = [] """
+
+#Inicialización lista vacía
+lista = []
+
+#Agregamos elementos a la lista vacía, mediante el método append
+lista.append("tenedor")
+lista.append("cuchillo")
+lista.append("plato")
+#Imprimimos la lista
+print(lista)
